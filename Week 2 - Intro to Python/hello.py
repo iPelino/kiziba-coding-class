598dc2394b6bb43516f702d4eb67e2cfb2362825
Pelin Mutanguha <pelmut2000@gmail.com>
2020-12-11 12:38:59 +0200

    Added week 2 content

---
+++ Week 2 - Intro to Python/hello.py
@@ -0,0 +1,43 @@
+# # My first python code
+# # fhgfg
+# print("300") # this will print 300 but it's a string
+# print(300) # this will also print 300 but it will be a number
+
+# print("Hello ", end=" ")
+# print("WOrld")
+
+# print("Hello World")
+# """ this
+# is a
+# multi-line
+# comment
+#  """
+
+# print("Let's begin")
+
+full_name_test = "Pelin Mutanguha" # snake_case
+fullName2 ="" # CamelCase
+FullName3 ="" # Pascal Case
+
+number1 = 100.0
+
+
+print(type(number1))
+complex_number = complex(10, 20)
+print(complex_number)
+
+print(False)
+
+name = 'James Bwimba' # or "James"
+# print(len(name))
+# print(name[len(name) -1])
+# print(name[-5])
+# print(name[6:12])
+# print(name[6:])
+
+# name_input = input("Enter your name: ")
+test = "Hello World"
+print(len(test))
+print(test[6:10])
+
+
---
+++ Week 2 - Intro to Python/intro.py
@@ -0,0 +1,13 @@
+# print(30, 40, "Hello World")
+
+# print("Hello" ,end=" ") 
+
+# print("World") 
+
+# my_string = "This is my string!"
+# print(my_string[0:7])
+# print(my_string[0:7:2])
+# print(my_string[len(my_string) - 1])
+
+first_number = int(input("Enter first number "))
+print(first_number)
